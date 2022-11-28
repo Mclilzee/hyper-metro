7c15e50a4f6ab8b6faf5e48c31a584026115ae0d
Emad Ali <mc.lilzee@hotmail.com>
2022-11-28 11:27:54 +0100

    Add tests to metro file reading

--- src/main/java/hyper/Main.java
+++ src/main/java/hyper/Main.java
@@ -1,7 +1,22 @@
 package hyper;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> metroList = readDataFromFile(args[0]);
+    }
+
+    private static List<String> readDataFromFile(String file) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+
+            return List.of();
+        } catch (IOException ex) {
+            System.out.println("Error! Such a file doesn't exist!");
+            return List.of();
+        }
     }
 }
--- src/test/java/hyper/MainTest.java
+++ src/test/java/hyper/MainTest.java
@@ -1,14 +1,35 @@
 package hyper;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
+    static ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    @BeforeAll
+    static void init() {
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @BeforeEach
+    void setup() {
+        outputStream.reset();
+    }
 
     @Test
     void printErrorIfFileNotFound() {
+        String[] testFile = {"nonexistance.txt"};
+        Main.main(testFile);
+
+        String expected = "Error! Such a file doesn't exist!\r\n";
+        assertEquals(expected, outputStream.toString());
 
     }
 
