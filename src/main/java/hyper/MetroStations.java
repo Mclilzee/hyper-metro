df0aa324b868dff03c065152a6ff7937566f80ab
Emad Ali <mc.lilzee@hotmail.com>
2022-11-28 13:58:25 +0100

    Add connectivity tests

--- src/main/java/hyper/MetroStations.java
+++ src/main/java/hyper/MetroStations.java
@@ -1,13 +1,32 @@
 package hyper;
 
-public class MetroStations {
-    private Station head = new Station("depot");
-    private Station nextStation;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
-    public void addStation(Station station) {
+public class MetroStations {
+    private final Station head = new Station("depot");
+
+    public MetroStations add(Station station) {
+        getLastStation().setNextStation(station);
+
+        return this;
     }
 
     public Station getHead() {
         return head;
     }
+
+    private Station getLastStation() {
+        Station station = head;
+        while (true) {
+           if (station.getNextStation() == null) {
+               return station;
+           }
+
+           station = station.getNextStation();
+        }
+    }
+
 }
---
+++ src/test/java/hyper/MetroStationsTest.java
@@ -0,0 +1,47 @@
+package hyper;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MetroStationsTest {
+
+    MetroStations metroStations = new MetroStations();
+
+    @Test
+    @DisplayName("Metro start with depot main station")
+    void metroInit() {
+        Station head = metroStations.getHead();
+
+        String expectedHeadName = "depot";
+
+        assertEquals(expectedHeadName, head.getName());
+    }
+
+    @Test
+    @DisplayName("Adding station makes head reference it")
+    void addStation() {
+        Station berlin = new Station("Berlin");
+        metroStations.add(berlin);
+
+        Station nextStation = metroStations.getHead().getNextStation();
+        String expected = "Berlin";
+
+        assertEquals(expected, nextStation.getName());
+    }
+
+    @Test
+    @DisplayName("Add multiple stations connect correctly")
+    void addMultipleStations() {
+        Station berlin = new Station("Berlin");
+        Station bremen = new Station("Bremen");
+        metroStations.add(berlin).add(bremen);
+
+        Station nextStation = metroStations.getHead().getNextStation();
+        Station finalStation = nextStation.getNextStation();
+        String expected = "Bremen";
+
+        assertEquals(expected, finalStation.getName());
+    }
+}
