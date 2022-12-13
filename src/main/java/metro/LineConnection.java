90e227c278cee28db06a01a5dcf20b981203fe09
Emad Ali <mc.lilzee@hotmail.com>
2022-12-13 15:38:20 +0100

    Refactor methods

--- src/main/java/metro/LineConnection.java
+++ src/main/java/metro/LineConnection.java
@@ -2,4 +2,7 @@ package metro;
 
 public record LineConnection(String stationName, String metroStationsName) {
 
+    public String getFullName() {
+        return String.format("%s (%s)", stationName, metroStationsName);
+    }
 }
--- src/main/java/metro/printing/LineConnectionsPrinter.java
+++ src/main/java/metro/printing/LineConnectionsPrinter.java
@@ -15,31 +15,22 @@ public class LineConnectionsPrinter implements MetroPrinter{
         }
 
         metroStations.getStationsConnection().stream()
-                .flatMap(station -> getStationString(station).stream())
+                .flatMap(station -> getStationFullStrings(station).stream())
                 .forEach(System.out::println);
 
         System.out.println("depot");
     }
 
-    private List<String> getStationString(Station station) {
-        if (station.getLineConnections().isEmpty()) {
-            return List.of(station.getName());
-        } else {
-            return getLineConnectionsList(station);
-        }
-    }
-
-    private List<String> getLineConnectionsList(Station station) {
+    private List<String> getStationFullStrings(Station station) {
         String stationName = station.getName();
+        List<LineConnection> connections = station.getLineConnections();
+
+        if (connections.isEmpty()) {
+            return List.of(stationName);
+        }
 
         return station.getLineConnections().stream()
-                .map(connection -> formatLineConnectionString(stationName, connection))
-                .toList();
-
-    }
-
-    private String formatLineConnectionString(String stationName, LineConnection lineConnection) {
-        return String.format("%s - %s (%s)", stationName, lineConnection.stationName(), lineConnection.metroStationsName());
-
+                      .map(connection -> stationName + " - " + connection.getFullName())
+                      .toList();
     }
 }
