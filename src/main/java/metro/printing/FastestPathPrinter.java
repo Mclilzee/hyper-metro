5553affac4d3ef365a790ad540cc2cd09bf50ca5
Emad Ali <mc.lilzee@hotmail.com>
2022-12-19 17:20:58 +0100

    Create fastest path printer

---
+++ src/main/java/metro/printing/FastestPathPrinter.java
@@ -0,0 +1,13 @@
+package metro.printing;
+
+import metro.search.Node;
+
+import java.util.List;
+
+public class FastestPathPrinter implements PathPrinter {
+
+    @Override
+    public String getPathString(List<Node> nodes) {
+        return null;
+    }
+}
---
+++ src/test/java/metro/printing/FastestPathPrinterTest.java
@@ -0,0 +1,63 @@
+package metro.printing;
+
+import metro.Station;
+import metro.search.Node;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FastestPathPrinterTest {
+
+    PathPrinter printer = new FastestPathPrinter();
+
+    @Test
+    void getCorrectPathString() {
+        Station berlin = new Station("Berlin", 0);
+        Station bremen = new Station("Bremen", 0);
+        Station frankfurt = new Station("Frankfurt", 0);
+
+        List<Node> nodes = List.of(new Node(berlin), new Node(bremen), new Node(frankfurt));
+        String actual = printer.getPathString(nodes);
+
+        String expected = """
+                          Berlin
+                          Bremen
+                          Frankfurt""";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void returnEmptyIfConnectionNotFound() {
+        String actual = printer.getPathString(List.of());
+
+        String expected = "";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Ger correct lines if stations has connections")
+    void correctStringLineConnections() {
+        Station berlin = new Station("Berlin", 0);
+        Station bremen = new Station("Bremen", 0);
+        Station beirut = new Station("Beirut", 0);
+        Station paris = new Station("Paris", 0);
+
+        List<Node> nodes = List.of(new Node(berlin), new Node(bremen), new Node(beirut, "France"), new Node(paris));
+        String actual = printer.getPathString(nodes);
+
+        String expected = """
+                          Berlin
+                          Bremen
+                          Transition to line France
+                          Beirut
+                          Paris""";
+
+        assertEquals(expected, actual);
+    }
+
+}
