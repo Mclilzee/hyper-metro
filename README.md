8285e28174f95ffb664d8e759646137fff9a2258
Emad Ali <mc.lilzee@hotmail.com>
2023-03-07 20:18:16 +0100

    Update prompts for visual representation

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Hyper Metro System
+
+Hyper metro is a project I enjoyed working on a lot, however challenging in some area.
+
+It represents a connection of metro systems, and make use of searching algorithms where you can search for fastest, shortest routes to take from one station to another.
+
+Start by building the project with `gradle build`
+
+After launching the program
--- src/main/java/metro/Main.java
+++ src/main/java/metro/Main.java
@@ -13,8 +13,11 @@ public class Main {
     public static void main(String[] args) {
         Path path = Paths.get(args[0]);
         if (Files.exists(path)) {
+            System.out.printf("Loading metro: %s%n", path);
             MetroFileReader reader = new MetroMemoryFileReader();
-            new MetrosController(new Scanner(System.in), reader.loadMetroServiceFromFile(path)).start();
+            MetrosController controller = new MetrosController(new Scanner(System.in), reader.loadMetroServiceFromFile(path));
+            System.out.println("Metro Ready, Check README.md for commands");
+            controller.start();
         } else {
             System.out.println("Error! Such a file doesn't exist!");
         }
